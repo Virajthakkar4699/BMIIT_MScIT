6ce35d7b31c29f58e32885a2c82a7f6b29f5dcc9
Virajthakkar4699 <mahadevfoamviraj@gmail.com>
2019-02-24 22:29:17 +0530

    Minor changes

---
+++ Semester_2/C++/Practical 1/PR7.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class bank_account
+{
+	int account_no;
+	char name[20];
+	char type[8];
+	int balance;
+	
+	public:
+		void initial()
+		{
+		}
+		void deposit()
+		{
+		}
+		void withdraw()
+		{
+		}
+		void display()
+		{
+		}
+	
+}
+
+int main()
+{
+}
